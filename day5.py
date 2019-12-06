c3ce4c59cdd4a8c5545dc2d35d9acbda65b31a46
z4tz <z4tz87@gmail.com>
2019-12-06 02:17:20 +0100

    Day 5 complete!

--- day5.py
+++ day5.py
@@ -1,7 +1,5 @@
 from inputreader import aocinput
-from day2 import IntcodeComputer
 from typing import List
-from inspect import signature
 
 
 class TEST:
@@ -9,81 +7,87 @@ class TEST:
         self.intcode = intcode
         self.position = 0
         self.running = False
+        self.input = None
         self.log = []
-        self.opcodes = {1: self.addition,
-                        2: self.multiplication,
-                        3: self.insert,
-                        4: self.output,
-                        5: self.jumpIfTrue,
-                        6: self.jumpIfFalse,
-                        7: self.lessThan,
-                        8: self.equals,
-                        99: self.stop}
+        self.modes = [0, 0, 0]
+        self.opcodes = {1: [self.addition, 3],  # opcode for each method and how many parameters it takes
+                        2: [self.multiplication, 3],
+                        3: [self.insert, 1],
+                        4: [self.output, 1],
+                        5: [self.jump_if_true, 2],
+                        6: [self.jump_if_false, 2],
+                        7: [self.less_than, 3],
+                        8: [self.equals, 3],
+                        99: [self.stop, 0]}
 
-    def addition(self, modes):
-        value1 = self.intcode[self.position + 1] if modes[2] else self.intcode[self.intcode[self.position + 1]]
-        value2 = self.intcode[self.position + 2] if modes[1] else self.intcode[self.intcode[self.position + 2]]
-        value3 = self.intcode[self.position + 3]
-        self.intcode[value3] = value1 + value2
-        self.position += 4
+    def getValue(self, parameter):
+        return self.intcode[self.position + parameter] if self.modes[parameter-1] else self.intcode[self.intcode[self.position + parameter]]
 
-    def multiplication(self, modes):
-        value1 = self.intcode[self.position + 1] if modes[2] else self.intcode[self.intcode[self.position + 1]]
-        value2 = self.intcode[self.position + 2] if modes[1] else self.intcode[self.intcode[self.position + 2]]
-        value3 = self.intcode[self.position + 3]
-        self.intcode[value3] = value1 * value2
-        self.position += 4
+    def addition(self):
+        self.intcode[self.intcode[self.position + 3]] = self.getValue(1) + self.getValue(2)
+
+    def multiplication(self):
+        self.intcode[self.intcode[self.position + 3]] = self.getValue(1) * self.getValue(2)
 
     def get_input_value(self):
-        return int(input('Input value to the TEST: '))
+        if self.input is not None:
+            return self.input
+        else:
+            return int(input('Input value to the TEST: '))
 
-    def insert(self, modes):
-        value1 = self.intcode[self.position + 1]
-        self.intcode[value1] = self.get_input_value()
-        self.position += 2
+    def insert(self):
+        self.intcode[self.intcode[self.position + 1]] = self.get_input_value()
 
-    def output(self, modes):
-        value1 = self.intcode[self.position + 1]
-        self.log.append(value1)
-        self.position += 2
+    def output(self):
+        self.log.append(self.getValue(1))
 
-    def jumpIfTrue(self, modes):
-        value1 = self.intcode[self.position + 1] if modes[2] else self.intcode[self.intcode[self.position + 1]]
+    def jump_if_true(self):
+        if self.getValue(1):
+            self.position = self.getValue(2)
+            self.position -= 3  # correct for moving forward if jumping
 
-        if self.intcode[value1]:
-            self.position = self.intcode[self.position + 2]
+    def jump_if_false(self):
+        if not self.getValue(1):
+            self.position = self.getValue(2)
+            self.position -= 3  # correct for moving forward if jumping
 
-    def jumpIfFalse(self, modes):
-        if not self.intcode[self.position + 1]:
-            self.position = self.intcode[self.position + 2]
+    def less_than(self):
+        self.intcode[self.intcode[self.position + 3]] = int(self.getValue(1) < self.getValue(2))
 
-    def lessThan(self, modes):
-        pass
+    def equals(self):
+        self.intcode[self.intcode[self.position + 3]] = int(self.getValue(1) == self.getValue(2))
 
-    def equals(self, modes):
-        pass
-
-    def stop(self, modes):
+    def stop(self):
         self.running = False
-        self.position += 1
 
     def run(self):
         self.running = True
         while self.running:
             opcode = f"{self.intcode[self.position]:05d}"  # get leading zeros visible
             try:
-                instruction = self.opcodes[int(opcode[-2:])]
+                instruction = self.opcodes[int(opcode[-2:])][0]
+                self.modes = [int(i) for i in reversed(opcode[:3])]
+                instruction()
+                self.position += self.opcodes[int(opcode[-2:])][1] + 1
             except KeyError:
                 print('Error, invalid opcode: ', opcode)
                 exit()
-            instruction([int(i) for i in opcode[:3]])
 
 
 def main(day):
     data = aocinput(day)
     data = [int(value) for value in data[0].split(',')]
-    test = TEST(data)
+
+    test = TEST(data.copy())
+    test.input = 1
     test.run()
+    print(test.log[-1])
+
+    # part 2
+    test = TEST(data.copy())
+    test.input = 5
+    test.run()
+    print(test.log[-1])
 
 
 if __name__ == '__main__':
--- tests/day5_tests.py
+++ tests/day5_tests.py
@@ -3,11 +3,105 @@ from day5 import TEST
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        arr = [1002,4,3,4,33]
+    def test_part1(self):
+        arr = [1002, 4, 3, 4, 33]
         test = TEST(arr)
         test.run()
-        self.assertEqual(test.intcode, [1002,4,3,4,99])
+        self.assertEqual(test.intcode, [1002, 4, 3, 4, 99])
+
+    def test_part2_equal_8_true(self):
+        arr = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+        test = TEST(arr)
+        test.input = 8
+        test.run()
+        self.assertEqual(test.log, [1])
+
+    def test_part2_equal_8_false(self):
+        arr = [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
+        test = TEST(arr)
+        test.input = 5
+        test.run()
+        self.assertEqual(test.log, [0])
+
+    def test_part2_less_than_8_true(self):
+        arr = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+        test = TEST(arr)
+        test.input = 7
+        test.run()
+        self.assertEqual(test.log, [1])
+
+    def test_part2_less_than_8_false(self):
+        arr = [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8]
+        test = TEST(arr)
+        test.input = 9
+        test.run()
+        self.assertEqual(test.log, [0])
+
+    def test_part2_imm_equal_8_true(self):
+        arr = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+        test = TEST(arr)
+        test.input = 8
+        test.run()
+        self.assertEqual(test.log, [1])
+
+    def test_part2_imm_equal_8_false(self):
+        arr = [3, 3, 1108, -1, 8, 3, 4, 3, 99]
+        test = TEST(arr)
+        test.input = 5
+        test.run()
+        self.assertEqual(test.log, [0])
+
+    def test_part2_imm_less_than_8_true(self):
+        arr = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+        test = TEST(arr)
+        test.input = 7
+        test.run()
+        self.assertEqual(test.log, [1])
+
+    def test_part2_imm_less_than_8_false(self):
+        arr = [3, 3, 1107, -1, 8, 3, 4, 3, 99]
+        test = TEST(arr)
+        test.input = 9
+        test.run()
+        self.assertEqual(test.log, [0])
+
+    def test_part2_pos_inp_zero(self):
+        arr = [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]
+        test = TEST(arr)
+        test.input = 0
+        test.run()
+        self.assertEqual(test.log, [0])
+
+    def test_part2_pos_inp_nonzero(self):
+        arr = [3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9]
+        test = TEST(arr)
+        test.input = 5
+        test.run()
+        self.assertEqual(test.log, [1])
+
+    def test_part2_large_below8(self):
+        arr = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125,
+               20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+        test = TEST(arr)
+        test.input = 7
+        test.run()
+        self.assertEqual(test.log, [999])
+
+    def test_part2_large_equal8(self):
+        arr = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125,
+               20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+        test = TEST(arr)
+        test.input = 8
+        test.run()
+        self.assertEqual(test.log, [1000])
+
+    def test_part2_large_above8(self):
+        arr = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125,
+               20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+        test = TEST(arr)
+        test.input = 9
+        test.run()
+        self.assertEqual(test.log, [1001])
 
 
 if __name__ == '__main__':
