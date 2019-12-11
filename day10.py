2b0559a7656b65b473ddc81dd02d58c902f094c5
z4tz <z4tz87@gmail.com>
2019-12-11 01:29:01 +0100

    Adding day 10, working version but needs refactoring, ugly!

--- day10.py
+++ day10.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 from inputreader import aocinput
 from typing import List, Tuple, Dict
-from fractions import Fraction
-from math import gcd
+from math import gcd, sin, pi, asin
 
 
 class Asteroid:
@@ -11,64 +10,102 @@ class Asteroid:
         self.in_los = set()
 
     @property
-    def x(self):
+    def x(self) -> int:
         return int(self.coordinates.real)
 
     @property
-    def y(self):
+    def y(self) -> int:
         return int(self.coordinates.imag)
 
-    def add_detectable(self, asteroid: Asteroid)->None:
+    def add_detectable(self, asteroid: Asteroid) -> None:
         self.in_los.add(asteroid)
+
+    def clear_detected(self):
+        self.in_los.clear()
 
     def __repr__(self):
         return f"({self.x}, {self.y})"
 
 
-def get_coordinates(data: List[str])-> List[complex]:
-    return [complex(x,y) for y, line in enumerate(data) for x, char in enumerate(line) if char == '#']
+def get_coordinates(data: List[str]) -> List[complex]:
+    return [complex(x, y) for y, line in enumerate(data) for x, char in enumerate(line) if char == '#']
 
 
-def detector_location(data: List[str])->int:
-    asteroids: Dict[complex, Asteroid] = {coord: Asteroid(coord) for coord in get_coordinates(data)}
+def get_distance(a1: Asteroid, a2: Asteroid) -> float:
+    return ((a1.x-a2.x)**2 + (a1.y-a2.y)**2)**0.5
 
-    def in_los(a1: Asteroid, a2: Asteroid)->bool:
+
+def get_angle(a1: Asteroid, a2: Asteroid) -> float:
+    dy = a1.y - a2.y
+    distance = get_distance(a1, a2)
+    return asin(dy/distance) / pi * 180
+
+
+def best_location(data: List[str]) -> Tuple[int, int]:
+    asteroids = {coord: Asteroid(coord) for coord in get_coordinates(data)}
+
+    def in_los(a1: Asteroid, a2: Asteroid) -> bool:
         if a1 is a2:
             return False
-        if a2.y-a1.y == 0:
-            xcoords = range(a1.x + 1, a2.x, 1)
+        if a2.y - a1.y == 0:
+            xcoords = range(a1.x + 1, a2.x, -int((a1.x-a2.x)/abs(a1.x-a2.x)))
             ycoords = [a1.y] * len(xcoords)  # same length list for zip
         elif a2.x - a1.x == 0:
-            ycoords = range(a1.y + 1, a2.y, 1)
+            ycoords = range(a1.y + 1, a2.y, -int((a1.y-a2.y)/abs(a1.y-a2.y)))
             xcoords = [a1.x] * len(ycoords)  # same length list for zip
         else:
-            print('case 3', a1, a2)
-            frac = Fraction(a2.x-a1.x, a2.y - a1.y)
-            dx = frac.numerator
-            dy = frac.denominator
-            print(dx, dy)
+
+            dx = a2.x - a1.x
+            dy = a2.y - a1.y
+            g = gcd(dx, dy)
+            dx //= g
+            dy //= g
+
             xcoords = range(a1.x + dx, a2.x, dx)
             ycoords = range(a1.y + dy, a2.y, dy)
-
         for x, y in zip(xcoords, ycoords):
             if complex(x, y) in asteroids:
                 return False
         return True
 
-    #for a1 in asteroids.values():
-    a1 = asteroids[3+4j]
-    for a2 in asteroids.values():
-        if in_los(a1, a2):
-            a1.add_detectable(a2)
-            a2.add_detectable(a1)
-    #for a in asteroids.values():
-    #    print(a)
-    return len(max(asteroids.values(), key=lambda x: len(x.in_los)).in_los)
+    for a1 in asteroids.values():
+        for a2 in asteroids.values():
+            if in_los(a1, a2):
+                a1.add_detectable(a2)
+                a2.add_detectable(a1)
+    best = max(asteroids.values(), key=lambda x: len(x.in_los))
+    result1 = len(best.in_los)
+
+    # part 2
+    asteroids = list(asteroids.values())
+    removed = []
+    while len(removed) < 200:
+
+        firstHalf = []
+        secondHalf = []
+
+        for a in best.in_los:
+            if a.x >= best.x:
+                firstHalf.append((a, get_angle(best, a)))
+            else:
+                secondHalf.append((a, get_angle(best, a)))
+        firstHalf.sort(key=lambda x: x[1], reverse=True)
+        secondHalf.sort(key=lambda x: x[1])
+        removed.extend(item[0] for item in firstHalf)
+        removed.extend(item[0] for item in secondHalf)
+        for a1 in firstHalf + secondHalf:
+            asteroids.remove(a1[0])
+        best.clear_detected()
+        for a2 in asteroids:
+            if in_los(best, a2):
+                best.add_detectable(a2)
+
+    return result1, removed[199].x*100 + removed[199].y
 
 
 def main(day):
     data = aocinput(day)
-    result = detector_location(data)
+    result = best_location(data)
     print(result)
 
 
--- tests/day10_tests.py
+++ tests/day10_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from day10 import detector_location
+from day10 import best_location, get_distance, get_angle, Asteroid
 
 
 class MyTestCase(unittest.TestCase):
@@ -11,6 +11,63 @@ class MyTestCase(unittest.TestCase):
                  "...##"]
 
         self.assertEqual(8, detector_location(setup))
+
+    def test_line_of_sight_2(self):
+        setup = ["......#.#.",
+                 "#..#.#....",
+                 "..#######.",
+                 ".#.#.###..",
+                 ".#..#.....",
+                 "..#....#.#",
+                 "#..#....#.",
+                 ".##.#..###",
+                 "##...#..#.",
+                 ".#....####"]
+        self.assertEqual(33, best_location(setup))
+
+    def test_distance(self):
+        a1 = Asteroid(0 + 0j)
+        a2 = Asteroid(1 + 0j)
+        a3 = Asteroid(-3 - 4j)
+        self.assertEqual(1, get_distance(a1, a2))
+        self.assertEqual(5, get_distance(a1, a3))
+
+    def test_angle(self):
+        a1 = Asteroid(0 + 0j)
+        a2 = Asteroid(1 + 0j)
+        a3 = Asteroid(1 + 1j)
+        a4 = Asteroid(-1 + 1j)
+        a5 = Asteroid(-1 - 1j)
+        a6 = Asteroid(0 + 1j)
+        self.assertEqual(0, get_angle(a1, a2))
+        self.assertAlmostEqual(45, get_angle(a1, a3))
+        self.assertAlmostEqual(45, get_angle(a1, a4))
+        self.assertAlmostEqual(-45, get_angle(a1, a5))
+        self.assertAlmostEqual(90, get_angle(a1, a6))
+
+    def test_part2(self):
+        setup = [".#..##.###...#######",
+                 "##.############..##.",
+                 ".#.######.########.#",
+                 ".###.#######.####.#.",
+                 "#####.##.#.##.###.##",
+                 "..#####..#.#########",
+                 "####################",
+                 "#.####....###.#.#.##",
+                 "##.#################",
+                 "#####.##.###..####..",
+                 "..######..##.#######",
+                 "####.##.####...##..#",
+                 ".#####..#.######.###",
+                 "##...#.##########...",
+                 "#.##########.#######",
+                 ".####.#.###.###.#.##",
+                 "....##.##.###..#####",
+                 ".#.#.###########.###",
+                 "#.#.#.#####.####.###",
+                 "###.##.####.##.#..##"]
+
+        self.assertEqual(802 ,best_location(setup)[1])
 
 
 if __name__ == '__main__':
