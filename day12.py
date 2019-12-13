b886bfc8eb896e5d571b5dcba12c8eed67ea2fd6
jimmal <jimmy.malmqvist@maxiv.lu.se>
2019-12-13 19:33:03 +0100

    day 12 ongoing

--- day12.py
+++ day12.py
@@ -3,12 +3,15 @@ from typing import Tuple, List
 import re
 import numpy as np
 from matplotlib import pyplot as plt
+from math import gcd
 
 
 class Moon:
     def __init__(self, x: int, y: int, z: int):
         self._position = np.array([x, y, z], dtype=int)
         self._velocity = np.zeros(3, dtype=int)
+        self._initial_position = self._position.copy()
+        self._periodicity = [0, 0, 0]
 
     def move(self):
         self._position += self._velocity
@@ -27,19 +30,41 @@ class Moon:
     def total_energy(self):
         return abs(self._position).sum() * abs(self._velocity).sum()
 
-    # todo: look at periodicity for each of the position directions
-    # todo: compare for all planets and get lowest common denominator?
+    def check_periodicity(self, steps: int):
+        for i in range(3):
+            if not self._periodicity[i] and self._position[i] == self._initial_position[i] and self._velocity[i] == 0:
+                self._periodicity[i] = steps
+
+    def periods_found(self):
+        return self._periodicity.count(0) == 0
 
 
-def total_energy(data: List[str], steps: int =2000, debug: bool = False) -> int:
+def total_energy(data: List[str], steps: int =7000, debug: bool = False) -> Tuple[int, int]:
     moons = [Moon(*re.match('<x=([-\d]*), y=([-\d]*), z=([-\d]*)>', line).groups()) for line in data]
+    print(moons[1]._position)
     for i in range(steps):
         step(moons)
+        for moon in moons:
+            if not moon.periods_found():
+                moon.check_periodicity(i+1)
+        for d in range(3):
+            if back_to_start(moons, d):
+                print(i, 'hello')
         if debug:
             print('------', i+1, '----------')
             for moon in moons:
                 print(moon.position, moon._velocity, moon.total_energy)
+    periods = []
+    for i in range(3):
+        periods.append(least_common_multiple([moon._periodicity[i] for moon in moons]))
+    print(least_common_multiple(periods))
+
     return sum(moon.total_energy for moon in moons)
+
+
+def back_to_start(moons, i):
+    return sum([moon._position[i] == moon._initial_position[i] and moon._velocity == 0 for moon in moons]) == 4
+
 
 
 def step(moons):
@@ -52,9 +77,19 @@ def step(moons):
         moon.move()
 
 
+def least_common_multiple(numbers: List[int]):
+    """
+    Finds the least common multiple for all numbers in list
+    """
+    lcm = numbers[0]
+    for number in numbers:
+        lcm = lcm*number // gcd(lcm, number)
+    return lcm
+
+
 def main(day):
     data = aocinput(day)
-    result = total_energy(data)
+    result = total_energy(data, debug=False)
     print(result)
 
 
--- tests/day12_tests.py
+++ tests/day12_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from day12 import total_energy
+from day12 import total_energy, least_common_multiple
 
 
 class MyTestCase(unittest.TestCase):
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
                 "<x=4, y=-8, z=8>",
                 "<x=3, y=5, z=-1>"]
 
-        self.assertEqual(False, total_energy(data, 10, debug=True))
+        self.assertEqual(179, total_energy(data, 10, debug=True))
 
     def test_energy(self):
         data = ["<x=-8, y=-10, z=0>",
@@ -18,6 +18,11 @@ class MyTestCase(unittest.TestCase):
                 "<x=9, y=-8, z=-3>"]
 
         self.assertEqual(1940, total_energy(data, 100, debug=True))
+
+    def test_lcm(self):
+        self.assertEqual(30, least_common_multiple([5, 10, 15]))
+        self.assertEqual(30, least_common_multiple([15, 10, 5]))
+        self.assertEqual(10, least_common_multiple([10, 10, 1]))
 
 
 if __name__ == '__main__':
