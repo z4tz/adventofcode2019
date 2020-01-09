7c4897807f36f2d6d31acbbd84eb6e3ff0427dd4
jimmal <jimmy.malmqvist@maxiv.lu.se>
2020-01-09 16:31:59 +0100

    day 16 part 2 complete

--- day16.py
+++ day16.py
@@ -1,65 +1,61 @@
 from inputreader import aocinput
 from typing import List
-from itertools import cycle
-
-base = [0, 1, 0, -1]
+from itertools import accumulate
 
 
-def patternGen(n):
-    return cycle([num for num in base for _ in range(n)])
-
-
-def FFT(signal, phases=100):
+def FFT(signal: List[int], phases: int =100) -> str:
+    return
     for i in range(phases):
-        signal = phase2(signal)
+        signal = phase(signal)
     return ''.join(map(str, signal[0:8]))
 
 
 def phase(signal: List[int]):
     output = []
-    for i in range(1, len(signal) + 1):
-        gen = patternGen(i)
-        next(gen)  # throw away first value
-        result = abs(sum([next(gen) * value for value in signal])) % 10
-        output.append(result)
-    return output
-
-
-def phase2(signal):
-    output = []
     length = len(signal)
     for i in range(1, length + 1):
-        print(i)
         add = 0
         sub = 0
 
         counter = -1
-        try:
-            while counter < length:
-                counter += i
-                add += sum(signal[counter:counter + i])
-                counter += i*2
-                sub += sum(signal[counter:counter + i])
-                counter += i
-
-        except IndexError:
-            pass
+        while counter < length:
+            counter += i
+            add += sum(signal[counter:counter + i])
+            counter += i*2
+            sub += sum(signal[counter:counter + i])
+            counter += i
         output.append(abs(add - sub) % 10)
 
     return output
 
 
 def decode(signal):
-    signal = signal*10000
-    print(FFT(signal), 1)
+    offset = int(''.join(str(number) for number in signal[0:7]))
+
+    if offset < len(signal)*10000 / 2:
+        print('This solution method does not work')
+        return
+
+    rev_signal = list(reversed((signal*10000)[offset:]))
+    for _ in range(100):
+        tot = 0
+        new_signal = []
+
+        for num in rev_signal:
+            tot = (tot + num) % 10
+            new_signal.append(tot)
+        rev_signal = new_signal
+
+    return ''.join(map(str, reversed(rev_signal[-8:])))
 
 
 def main(day):
     data = aocinput(day)
     data = [int(part) for part in data[0].strip()]
-    #result = FFT(data)
+    result = FFT(data)
     result2 = decode(data)
-    #print(result)
+    print(result, result2)
+
 
 
 if __name__ == '__main__':
